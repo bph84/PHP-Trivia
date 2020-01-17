0ee9882a2a8a6e895445028246d0694b2835dc15
Ben Heley <github-bph@bph.me>
2020-01-17 19:09:30 +0000

    Update start.php
    A couple more tests. Contains text rather than matches.

--- start.php
+++ start.php
@@ -9,15 +9,33 @@ expectObContent(function() {
     echo "testing here";
 }, "testing here");
 
+expectObContent(function() {
+    print(1 + "1"); //FML moment there;
+}, "2");
 
-function expectObContent($outputtingFunc, $expectedText) {
+expectObContent(function() {
+    print(1 . "1"); //FML moment there;
+}, "11");
+
+expectObContent(function() {
+    print($notARealVariable); //FML moment there;
+}, "Undefined variable: notARealVariable in");
+
+function expectObContent($outputtingFunc, $expectedText) { 
+    // expected text within the output text since PHP appents formatting stuff <br /> <b> etc. 
+
     ob_start();
     $outputtingFunc();
     $result = ob_get_contents();
     ob_end_clean();
 
-    assert($result == $expectedText);
+    assert(strpos($result, $expectedText) !== false);
+    if (strpos($result, $expectedText) === false) {
+        var_dump($result);
+        var_dump($expectedText);
+    }
 }
+
 
 
 
